493fff268a81c21b89b393fd5f213c4ed4e4ee66
GodWriter <18917565247@163.com>
2020-10-10 11:47:03 +0800

    commit.

---
+++ test.py
@@ -0,0 +1,35 @@
+import os
+import time
+import torch
+import argparse
+
+from utils import parse_data_config
+from dataloader import DataLoader
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+    parser.add_argument("--data_config", type=str, default="config/ships/702.data", help="path to data config file")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+
+    opt = parser.parse_args()
+    print(opt)
+
+    data_config = parse_data_config(opt.data_config)
+    train_path = data_config["train"]
+    test_path = data_config["test"]
+    class_names = load_classes(data_config["name"])
+
+    dataset = DataLoader(train_path, augment=True, multiscale=opt.multiscale_training)
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=opt.batch_size,
+                                             shuffle=True,
+                                             num_workers=0,
+                                             pin_memory=False,
+                                             collate_fn=dataset.collate_fn)
+    
+    for batch_i, (images, labels) in enumerate(dataloader):
+        print("batch_i: ", batch_i)
+        print("images: ", images.shape)
---
+++ utils.py
@@ -0,0 +1,17 @@
+def parse_data_config(path):
+    options = dict()
+
+    options['gpus'] = '0, 1, 2, 3'
+    options['num_workers'] = '0'
+
+    with open(path, 'r') as fp:
+        lines = fp.readlines()
+
+    for line in lines:
+        line = line.strip()
+        if line == '' or line.startswith('#'):
+            continue
+        key, value = line.split('=')
+        options[key.strip()] = value.strip()
+
+    return options
