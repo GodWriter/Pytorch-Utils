9f3a1e65e1c06dcca3df65ef7efc2b32f5ac1935
GodWriter <18917565247@163.com>
2020-10-12 15:42:44 +0800

    commit.

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import os
+import cv2
+import numpy as np
 import xml.etree.ElementTree as ET
 
+from PIL import Image
 from xml.dom.minidom import Document
+from matplotlib import pyplot as plt
 
 
 def load_classes(path):
@@ -80,13 +84,33 @@ def xml2txt(xml_path, txt_path):
 
                 bbox[0] = round(center_x / width, 6)
                 bbox[1] = round(center_y / height, 6)
-                bbox[2] = round(center_x / width, 6)
-                bbox[3] = round(center_y / height, 6)
+                bbox[2] = round(w / width, 6)
+                bbox[3] = round(h / height, 6)
 
                 for box in bbox:
                     line += ' ' + str(box)
                 line += '\n'
                 fp.writelines(line)
+
+
+def pltBbox(img_path, label_path):
+    img = cv2.imread(img_path)
+    boxes = np.loadtxt(label_path, dtype=np.float).reshape(-1, 5)
+    boxesXXYY = boxes.copy()
+    
+    # convert [c_x, c_y, w, h] to [xmin, ymin, xmax, ymax]
+    H, W, _ = img.shape
+    boxesXXYY[:, 1] = (boxes[:, 1] - boxes[:, 3] / 2) * W
+    boxesXXYY[:, 2] = (boxes[:, 2] - boxes[:, 4] / 2) * H
+    boxesXXYY[:, 3] = (boxes[:, 1] + boxes[:, 3] / 2) * W
+    boxesXXYY[:, 4] = (boxes[:, 2] + boxes[:, 4] / 2) * H
+
+    for box in boxesXXYY:
+        cv2.rectangle(img, (int(box[1]), int(box[2])), (int(box[3]), int(box[4])), (0, 255, 0), 2)
+
+    cv2.imshow(img_path, img)
+    cv2.waitKey()
+
 
 def parse_data_config(path):
     options = dict()
@@ -108,6 +132,8 @@ def parse_data_config(path):
     return options
 
 
-data_config = parse_data_config("config/ships/702.data")
-class_names = load_classes(data_config["name"])
-xml2txt("data/ships/xmls", "data/ships/labels")
+# data_config = parse_data_config("config/ships/702.data")
+# class_names = load_classes(data_config["name"])
+# xml2txt("data/ships/xmls", "data/ships/labels")
+
+# pltBbox("data/ships/images/1.jpg", "data/ships/labels/1.txt")
