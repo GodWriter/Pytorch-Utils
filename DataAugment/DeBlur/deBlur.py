5f77f8fc99ea5885d11961c8095ea566117445ad
GodWriter <18917565247@163.com>
2020-11-20 14:51:52 +0800

    commit.

---
+++ DataAugment/DeBlur/deBlur.py
@@ -0,0 +1,327 @@
+import os
+import cv2
+import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from math import ceil
+from scipy import misc
+from scipy import signal
+
+
+class Trajectory(object):
+    def __init__(self, canvas=64, iters=2000, max_len=60, expl=None, path_to_save=None):
+        """
+        :param canvas: size of domain where our trajectory os defined.
+        :param iters: number of iterations for definition of our trajectory.
+        :param max_len: maximum length of our trajectory.
+        :param expl: this param helps to define probability of big shake. Recommended expl = 0.005.
+        :param path_to_save: where to save if you need.
+        """
+        self.canvas = canvas
+        self.iters = iters
+        self.max_len = max_len
+    
+        if expl is None:
+            self.expl = 0.1 * np.random.uniform(0, 1)
+        else:
+            self.expl = expl
+
+        if path_to_save is None:
+            pass
+        else:
+            self.path_to_save = path_to_save
+
+        self.tot_length = None
+        self.big_expl_count = None
+        self.x = None
+
+    def fit(self, show=False, save=False):
+        """
+        :param show: default False.
+        :param save: default False.
+        :return: x (vector of motion).
+        """
+        tot_length = 0
+        big_expl_count = 0
+
+        # how to be near the previous position
+        # TODO: I can change this paramether for 0.1 and make kernel at all image
+        centripetal = 0.7 * np.random.uniform(0, 1)
+
+        # probability of big shake
+        prob_big_shake = 0.2 * np.random.uniform(0, 1)
+
+        # term determining, at each sample, the random component of the new direction
+        gaussian_shake = 10 * np.random.uniform(0, 1)
+        init_angle = 360 * np.random.uniform(0, 1)
+
+        img_v0 = np.sin(np.deg2rad(init_angle))
+        real_v0 = np.cos(np.deg2rad(init_angle))
+
+        v0 = complex(real=real_v0, imag=img_v0)
+        v = v0 * self.max_len / (self.iters - 1)
+
+        if self.expl > 0:
+            v = v0 * self.expl
+
+        x = np.array([complex(real=0, imag=0)] * (self.iters))
+
+        for t in range(0, self.iters - 1):
+            if np.random.uniform() < prob_big_shake * self.expl:
+                next_direction = 2 * v * (np.exp(complex(real=0, imag=np.pi + (np.random.uniform() - 0.5))))
+                big_expl_count += 1
+            else:
+                next_direction = 0
+
+            dv = next_direction + self.expl * (
+                gaussian_shake * complex(real=np.random.randn(), imag=np.random.randn()) - centripetal * x[t]) * (
+                                      self.max_len / (self.iters - 1))
+
+            v += dv
+            v = (v / float(np.abs(v))) * (self.max_len / float((self.iters - 1)))
+            x[t + 1] = x[t] + v
+            tot_length = tot_length + abs(x[t + 1] - x[t])
+
+        # centere the motion
+        x += complex(real=-np.min(x.real), imag=-np.min(x.imag))
+        x = x - complex(real=x[0].real % 1., imag=x[0].imag % 1.) + complex(1, 1)
+        x += complex(real=ceil((self.canvas - max(x.real)) / 2), imag=ceil((self.canvas - max(x.imag)) / 2))
+
+        self.tot_length = tot_length
+        self.big_expl_count = big_expl_count
+        self.x = x
+
+        if show or save:
+            self.__plot_canvas(show, save)
+        return self
+
+    def __plot_canvas(self, show, save):
+        if self.x is None:
+            raise Exception("Please run fit() method first")
+        else:
+            plt.close()
+            plt.plot(self.x.real, self.x.imag, '-', color='blue')
+
+            plt.xlim((0, self.canvas))
+            plt.ylim((0, self.canvas))
+            if show and save:
+                plt.savefig(self.path_to_save)
+                plt.show()
+            elif save:
+                if self.path_to_save is None:
+                    raise Exception('Please create Trajectory instance with path_to_save')
+                plt.savefig(self.path_to_save)
+            elif show:
+                plt.show()
+
+
+class PSF(object):
+    def __init__(self, canvas=None, trajectory=None, fraction=None, path_to_save=None):
+        if canvas is None:
+            self.canvas = (canvas, canvas)
+        else:
+            self.canvas = (canvas, canvas)
+        if trajectory is None:
+            self.trajectory = Trajectory(canvas=canvas, expl=0.005).fit(show=False, save=False)
+        else:
+            self.trajectory = trajectory.x
+        if fraction is None:
+            self.fraction = [1/100, 1/10, 1/2, 1]
+        else:
+            self.fraction = fraction
+        self.path_to_save = path_to_save
+        self.PSFnumber = len(self.fraction)
+        self.iters = len(self.trajectory)
+        self.PSFs = []
+
+    def fit(self, show=False, save=False):
+        PSF = np.zeros(self.canvas)
+
+        triangle_fun = lambda x: np.maximum(0, (1 - np.abs(x)))
+        triangle_fun_prod = lambda x, y: np.multiply(triangle_fun(x), triangle_fun(y))
+        for j in range(self.PSFnumber):
+            if j == 0:
+                prevT = 0
+            else:
+                prevT = self.fraction[j - 1]
+
+            for t in range(len(self.trajectory)):
+                # print(j, t)
+                if (self.fraction[j] * self.iters >= t) and (prevT * self.iters < t - 1):
+                    t_proportion = 1
+                elif (self.fraction[j] * self.iters >= t - 1) and (prevT * self.iters < t - 1):
+                    t_proportion = self.fraction[j] * self.iters - (t - 1)
+                elif (self.fraction[j] * self.iters >= t) and (prevT * self.iters < t):
+                    t_proportion = t - (prevT * self.iters)
+                elif (self.fraction[j] * self.iters >= t - 1) and (prevT * self.iters < t):
+                    t_proportion = (self.fraction[j] - prevT) * self.iters
+                else:
+                    t_proportion = 0
+
+                m2 = int(np.minimum(self.canvas[1] - 1, np.maximum(1, np.math.floor(self.trajectory[t].real))))
+                M2 = int(m2 + 1)
+                m1 = int(np.minimum(self.canvas[0] - 1, np.maximum(1, np.math.floor(self.trajectory[t].imag))))
+                M1 = int(m1 + 1)
+
+                PSF[m1, m2] += t_proportion * triangle_fun_prod(
+                    self.trajectory[t].real - m2, self.trajectory[t].imag - m1
+                )
+                PSF[m1, M2] += t_proportion * triangle_fun_prod(
+                    self.trajectory[t].real - M2, self.trajectory[t].imag - m1
+                )
+                PSF[M1, m2] += t_proportion * triangle_fun_prod(
+                    self.trajectory[t].real - m2, self.trajectory[t].imag - M1
+                )
+                PSF[M1, M2] += t_proportion * triangle_fun_prod(
+                    self.trajectory[t].real - M2, self.trajectory[t].imag - M1
+                )
+
+            self.PSFs.append(PSF / (self.iters))
+        if show or save:
+            self.__plot_canvas(show, save)
+
+        return self.PSFs
+
+    def __plot_canvas(self, show, save):
+        if len(self.PSFs) == 0:
+            raise Exception("Please run fit() method first.")
+        else:
+            plt.close()
+            fig, axes = plt.subplots(1, self.PSFnumber, figsize=(10, 10))
+            for i in range(self.PSFnumber):
+                axes[i].imshow(self.PSFs[i], cmap='gray')
+            if show and save:
+                if self.path_to_save is None:
+                    raise Exception('Please create Trajectory instance with path_to_save')
+                plt.savefig(self.path_to_save)
+                plt.show()
+            elif save:
+                if self.path_to_save is None:
+                    raise Exception('Please create Trajectory instance with path_to_save')
+                plt.savefig(self.path_to_save)
+            elif show:
+                plt.show()
+
+
+class BlurImage(object):
+
+    def __init__(self, image_path, image_name, PSFs=None, part=None, path__to_save=None):
+        """
+        :param image_path: path to square, RGB image.
+        :param PSFs: array of Kernels.
+        :param part: int number of kernel to use.
+        :param path__to_save: folder to save results.
+        """
+        self.path_to_save = path__to_save
+        self.image_name = image_name
+
+        if os.path.isfile(image_path):
+            self.image_path = image_path
+            self.original = misc.imread(self.image_path)
+            self.shape = self.original.shape
+            if len(self.shape) < 3:
+                raise Exception('We support only RGB images yet.')
+            elif self.shape[0] != self.shape[1]:
+                raise Exception('We support only square images yet.')
+        else:
+            raise Exception('Not correct path to image.')
+
+        if PSFs is None:
+            if self.path_to_save is None:
+                self.PSFs = PSF(canvas=self.shape[0]).fit()
+            else:
+                self.PSFs = PSF(canvas=self.shape[0], path_to_save=os.path.join(self.path_to_save,
+                                                                                'PSFs.png')).fit(save=True)
+        else:
+            self.PSFs = PSFs
+
+        self.part = part
+        self.result = []
+
+    def blur_image(self, save=False, show=False):
+        if self.part is None:
+            psf = self.PSFs
+        else:
+            psf = [self.PSFs[self.part]]
+
+        yN, xN, channel = self.shape
+        key, kex = self.PSFs[0].shape
+        delta = yN - key
+        assert delta >= 0, 'resolution of image should be higher than kernel'
+
+        result=[]
+        if len(psf) > 1:
+            for p in psf:
+                tmp = np.pad(p, delta // 2, 'constant')
+                cv2.normalize(tmp, tmp, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+                # blured = np.zeros(self.shape)
+                blured = cv2.normalize(self.original, self.original, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
+                                       dtype=cv2.CV_32F)
+                blured[:, :, 0] = np.array(signal.fftconvolve(blured[:, :, 0], tmp, 'same'))
+                blured[:, :, 1] = np.array(signal.fftconvolve(blured[:, :, 1], tmp, 'same'))
+                blured[:, :, 2] = np.array(signal.fftconvolve(blured[:, :, 2], tmp, 'same'))
+                blured = cv2.normalize(blured, blured, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+                blured = cv2.cvtColor(blured, cv2.COLOR_RGB2BGR)
+                result.append(np.abs(blured))
+        else:
+            psf = psf[0]
+            tmp = np.pad(psf, delta // 2, 'constant')
+            cv2.normalize(tmp, tmp, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            blured = cv2.normalize(self.original, self.original, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
+                                   dtype=cv2.CV_32F)
+            blured[:, :, 0] = np.array(signal.fftconvolve(blured[:, :, 0], tmp, 'same'))
+            blured[:, :, 1] = np.array(signal.fftconvolve(blured[:, :, 1], tmp, 'same'))
+            blured[:, :, 2] = np.array(signal.fftconvolve(blured[:, :, 2], tmp, 'same'))
+            blured = cv2.normalize(blured, blured, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            blured = cv2.cvtColor(blured, cv2.COLOR_RGB2BGR)
+            result.append(np.abs(blured))
+
+        self.result = result
+        if show or save:
+            self.__plot_canvas(show, save)
+
+    def __plot_canvas(self, show, save):
+        if len(self.result) == 0:
+            raise Exception('Please run blur_image() method first.')
+        else:
+            plt.close()
+            plt.axis('off')
+            fig, axes = plt.subplots(1, len(self.result), figsize=(10, 10))
+
+            if len(self.result) > 1:
+                for i in range(len(self.result)):
+                        axes[i].imshow(self.result[i])
+            else:
+                plt.axis('off')
+                plt.imshow(self.result[0])
+
+            if show and save:
+                if self.path_to_save is None:
+                    raise Exception('Please create Trajectory instance with path_to_save')
+                cv2.imwrite(os.path.join(self.path_to_save, self.image_name), self.result[0] * 255)
+                plt.show()
+            elif save:
+                if self.path_to_save is None:
+                    raise Exception('Please create Trajectory instance with path_to_save')
+                cv2.imwrite(os.path.join(self.path_to_save, self.image_name), self.result[0] * 255)
+            elif show:
+                plt.show()
+
+
+if __name__ == '__main__':
+    folder = 'data/A/train'
+    folder_to_save = 'data/B/train'
+    params = [0.01, 0.009, 0.008, 0.007, 0.005, 0.003]
+
+    for path in tqdm.tqdm(os.listdir(folder)):
+        trajectory = Trajectory(canvas=64, max_len=60, expl=np.random.choice(params)).fit()
+        psf = PSF(canvas=64, trajectory=trajectory).fit()
+
+        blurImg = BlurImage(os.path.join(folder, path),
+                            image_name=path,
+                            PSFs=psf,
+                            path__to_save=folder_to_save,
+                            part=np.random.choice([1, 2, 3]))
+        blurImg.blur_image(save=True)
