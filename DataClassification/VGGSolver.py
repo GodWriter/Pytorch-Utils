84816bd84916a4587c68bad8fe6a68be6c8d7cb7
GodWriter <18917565247@163.com>
2020-11-25 20:13:02 +0800

    commit.

--- DataClassification/VGGSolver.py
+++ DataClassification/VGGSolver.py
@@ -43,37 +43,52 @@ def main(args):
 
             loss = model.train(images, labels)
 
-            n_iter = (args.epochs - 1) * len(train_loader) + batch_i + 1
+            n_iter = epoch * len(train_loader) + batch_i + 1
             writer.add_scalar('Train/loss', loss, n_iter)
 
             print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]'.format(epoch=epoch,
                                                                                        trained_samples=batch_i*args.batch_size + len(images),
                                                                                        total_samples=len(train_loader.dataset)))
+        
+        if epoch % args.evaluation_interval == 0:
+            for batch_i, (images, labels) in enumerate(valid_loader):
+                images = images.to(args.device)
+                labels = labels.squeeze(1).to(args.device)
 
-        for batch_i, (images, labels) in enumerate(train_loader):
-            images = images.to(args.device)
-            labels = labels.squeeze(1).to(args.device)
+                c_3, c_1 = model.valid(images, labels)
 
-            c_3, c_1 = model.valid(images, labels)
+                correct_3 += c_3.item()
+                correct_1 += c_1.item()
+            
+            top_1_error = 1 - correct_1 / len(train_loader.dataset)
+            top_3_error = 1 - correct_3 / len(train_loader.dataset)
 
-            correct_3 += c_3
-            correct_1 += c_1
+            writer.add_scalar('Valid/Top_1_error', top_1_error, epoch)
+            writer.add_scalar('Valid/Top_3_error', top_3_error, epoch)
 
-        print("Top 1 err: ", 1 - correct_1 / len(train_loader.dataset))
-        print("Top 3 err: ", 1 - correct_3 / len(train_loader.dataset))
+            print("Top 1 err: ", top_1_error)
+            print("Top 3 err: ", top_3_error)
+
+        if epoch % args.checkpoint_interval == 0:
+            torch.save(model.vgg.state_dict(), f"checkpoints/vgg_%d.pth" % epoch)
+    
+    torch.save(model.vgg.state_dict(), f"checkpoints/vgg_done.pth")
+    writer.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
-    parser.add_argument("--n_cpu", type=int, default=8, help="dataloader threads number")
+    parser.add_argument("--n_cpu", type=int, default=4, help="dataloader threads number")
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
-    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--learning_rate', type=float, default=1e-6)
     parser.add_argument("--img_size", type=int, default=256, help="size of each image dimension")
     parser.add_argument("--num_class", type=int, default=4, help="number of classes you want to train")
     parser.add_argument("--vgg_type", type=int, default=11, help="you can choose from 11, 13, 16, 19")
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--logs", type=str, default="logs/Vgg")
+    parser.add_argument("--evaluation_interval", type=int, default=2, help="interval evaluations on validation set")
+    parser.add_argument("--checkpoint_interval", type=int, default=10, help="interval between saving model weights")
     parser.add_argument("--train_path", type=str, default="data/weather/train.txt", help="txt path saving image paths")
     parser.add_argument("--test_path", type=str, default="data/weather/test.txt", help="txt path saving image paths")
     args = parser.parse_args()
